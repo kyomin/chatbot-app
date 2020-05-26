5c7227ecb5e54e934cf03a03b1393c2c04fdedf3
kyomin <kim031504@naver.com>
2020-05-26 17:08:13 +0900

    Dialogflow API 설정 및 Text Query Route 만들기 완료

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const config = require("./server/config/keys");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+/* 앞의 반복되는 패턴의 url을 생략해서 작업할 수 있게 해준다. */
 app.use('/api/dialogflow', require('./server/routes/dialogflow'));
 
 // Serve static assets if in production
--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -4,80 +4,46 @@ const structjson = require('./structjson.js');
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
 
-const config = require('../config/keys');
+const config = require('../config/dev');
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
 
-const projectId = config.googleProjectID
-const sessionId = config.dialogFlowSessionID
-const languageCode = config.dialogFlowSessionLanguageCode
-
-
-// Create a new session
+// 내 계정을 이용하여 dialogflow와의 세션 생성
 const sessionClient = new dialogflow.SessionsClient();
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
-// We will make two routes 
+/* 두 개의 라우트를 만들 것이다. */
 
-
-// Text Query Route
-
-router.post('/textQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
+// 1. Text Query Route : 클라이언트에서 타이핑을 한 것에 대한 처리
+router.post('/textQuery', async (req, res) => {     // await 키워드는 async 함수 내에서 작동하므로 이렇게 명시!
+    // 클라이언트에서 받은 정보를 Dialogflow API에게 보내준다.
+      
     // The text query request.
     const request = {
         session: sessionPath,
         queryInput: {
             text: {
-                // The query to send to the dialogflow agent
+                // 클라이언트에서 타이핑한 쿼리가 dialogflow agent에게 보내진다. 하드코딩이 아닌, 어떤 쿼리든 여기로 와야 하므로 변수로 담는다.
                 text: req.body.text,
                 // The language used by the client (en-US)
                 languageCode: languageCode,
             },
         },
     };
-
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
+    
+    // Send request and log result to Dialogflow API
+    const responses = await sessionClient.detectIntent(request);    // Dialogflow에서 가공한 데이터가 담긴다.
     console.log('Detected intent');
     const result = responses[0].queryResult;
     console.log(`  Query: ${result.queryText}`);
     console.log(`  Response: ${result.fulfillmentText}`);
-
-    res.send(result)
-})
-
-
-
-//Event Query Route
-
-router.post('/eventQuery', async (req, res) => {
-    //We need to send some information that comes from the client to Dialogflow API 
-    // The text query request.
-    const request = {
-        session: sessionPath,
-        queryInput: {
-            event: {
-                // The query to send to the dialogflow agent
-                name: req.body.event,
-                // The language used by the client (en-US)
-                languageCode: languageCode,
-            },
-        },
-    };
-
-    // Send request and log result
-    const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-
-    res.send(result)
-})
+    
+    res.send(result);
+});
 
 
-
-
-
+// 2. Event Query Route : 타이핑 없이, 특정 이벤트에 대한 응답
 
 
 module.exports = router;
