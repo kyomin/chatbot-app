db36f752bbf94401fa39c8371c587868b04d3365
kyomin <kim031504@naver.com>
2020-05-28 03:25:25 +0900

    프론트 엔드 챗봇 창 틀 만들기 완료

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,11 @@ function App() {
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
-        <Title level={2} >CHAT BOT APP&nbsp;<Icon type="robot" /></Title>
+        <Title level={2} >KYO BOT APP&nbsp;<Icon type="robot" /></Title>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
        
         <Chatbot />
-
 
       </div>
     </div>
